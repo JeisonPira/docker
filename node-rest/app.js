d25347315958ec1d6f9ec8fdfd33de2fd477bbf4
Jeison <jeison@gmail.com>
2019-01-08 13:21:15 -0500

    mysql connection

--- node-rest/app.js
+++ node-rest/app.js
@@ -4,12 +4,37 @@ var express = require("express"),
     methodOverride = require("method-override");
 http = require("http"),
     server = http.createServer(app),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    mysql = require('mysql');
 
 mongoose.connect('mongodb://localhost:27017/tvshows', function (err, res) {
     if (err) throw err;
     console.log('Connected to Database');
 });
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_mysql',
+    port: 3306
+});
+
+connection.connect(function (error) {
+    if (error) throw error;
+    console.log('Conexion correcta.');
+});
+
+var query2 = connection.query('INSERT INTO testnode(testNodecol) VALUES (?);', ['testNodeCol2'], function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+
+var query = connection.query('SELECT * FROM testnode', function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+
 require('./models/tvshow');
 require('./controllers/tvshows');
 
